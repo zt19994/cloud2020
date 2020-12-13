b2ad702b7c9966bd2b0e0842dbea6b31e5a9523b
zt1994 <zt191610942@163.com>
2020-12-13 15:22:00 +0800

    构建payment子模块

---
+++ cloud-provider-payment8001/src/main/java/com/learn/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.learn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zt1994
+ * @date 2020/12/13 14:04
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
