7834cb6941d28e99266b617bcf275b28b1aaa8ad
zt1994 <zt191610942@163.com>
2020-12-13 22:37:21 +0800

    Eureka 集群配置

---
+++ cloud-eureka-server7002/src/main/java/com/learn/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.learn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zt1994
+ * @date 2020/12/13 22:28
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
